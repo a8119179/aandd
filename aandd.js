fc2e69236b796156cffdd73c7335ff788da3dacb
So Maekawa <>
2021-05-25 19:31:31 +0900

    prac3-1

--- aandd.js
+++ aandd.js
@@ -1,14 +1,20 @@
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
-    html = html + '</tr>';
-    html = html + '</table>';
-    //document.getElementById('res').innerHTML = html;
-    document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
 
 function createSequentialNumbers(array, n) {
@@ -19,6 +25,7 @@ function createSequentialNumbers(array, n) {
 
 function createRandomNumbers(array, n) {
     for (var i = 0; i < n; i++) {
-        array.push(Math.floor(Math.random() * 100));
+        var a = Math.floor(Math.random() * 100)
+        array.push(a);
     }
 }
